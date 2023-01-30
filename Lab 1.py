9cdf9f4f00e10adfa606337ce41cc425df6d5a32
Abdul-Azeem Opakunle <73696990+Opaksbolu@users.noreply.github.com>
2023-01-30 12:44:41 -0600

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,27 @@
+#Variables 
+#first name
+fname = "dave"
+#last name
+lname = "bautista"
+#last name alternate
+laname = "Bautista's"
+#birth year
+byear = 1969
+#favorite number
+fnumber = 7
+#favorite color
+fcolor = "Black"
+#current year
+cyear = 2021
+#age
+age = ((cyear)-(byear))
+#proper name
+pname = (fname.title(), lname.title())
+#favorate number squared
+ffnumber = (fnumber**2)
+#This program tells his age
+print(fname.title(), lname.title(),"is", age, "years old.")
+#This program tells his favorite color and number
+print(fname.title(), laname,"favorite color is", fcolor, "and favorite number is", fnumber)
+#This program tells his favorite number squared
+print("The square of the favorite number is ", ffnumber)
---
+++ Practice.py
@@ -0,0 +1,43 @@
+#Abdul-Azeem Opakunle
+#practice chapter 2
+
+#variables
+
+my_fruit = "peach"
+newfruit = "plum"
+
+print(my_fruit)
+print(newfruit)
+
+all_fruit = f"{my_fruit} and {newfruit}"
+print(all_fruit)
+
+#string methods
+first_name = "mike"
+last_name = "oxmall"
+
+print(first_name.title())
+print(first_name.upper())
+print(first_name.lower())
+
+#redapple
+
+password = 'password'
+
+#rstrip, lstrip, strip
+
+good_pass = password.lstrip()
+print(password)
+print(good_pass)
+
+message = "The captain said 'shiver ya timbers'. \nHe was a very jolly pirate captain."
+print(message)
+
+#numbers
+age = 17 
+height = 5.8
+print(23 + 6)
+
+# + - * / **
+
+6 + 4 * 2 / 2 + 12
