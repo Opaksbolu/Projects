d2a63e3956e6abb71b98368351c4b954041c6174
Abdul-Azeem Opakunle <73696990+Opaksbolu@users.noreply.github.com>
2023-02-25 23:22:06 -0600

    Add files via upload

---
+++ Week Two Practice.py
@@ -0,0 +1,37 @@
+#In this program I changed the variable fruit from grapes to banana
+2-2
+fruit = "Bananas"
+
+print("I love ", fruit)
+#In this program I set the variable name1 to represent the name Jason and put it in a sentence.
+2-3 
+name1 = "Jason"
+
+print("Hey,", name1, "would you like to learn how to use python?")
+#In ths program I set the variable first_name to be named Jay and then made it appear in lower, upper and title font.
+2-4
+first_name = "Jay"
+
+print(first_name.lower())
+print(first_name.upper())
+print(first_name.title())
+# I set the varible quote1 to state a statement and then stated the quoter before quote1
+2-5
+quote1 = '"The time is always right to do what is right."'
+print("Martin Luther King once said,", quote1)
+#I used all basic orders of operations to get all the answers to end up as 8.
+2-8
+
+print(2+6)
+print(16-8)
+print(2*4)
+print(24/3)
+#I used number1 as a variable to indicate my favourite number, then I included them in a statement.
+2-9
+
+number1 = "72"
+print("My name is Abdul and my favorite number is", number1, "and the reason I like it because its unique.")
+
+2-10
+
+#I used the pound symbol to create comments for all the programs written.
